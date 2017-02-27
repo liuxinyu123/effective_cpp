49ca7c4cbddcdda5c7ff6646a45a55c14df5de93
liuyakun <happy_lucky52@163.com>
2017-02-27 22:46:37 +0800

    item 16

---
+++ item16/main.cpp
@@ -0,0 +1,12 @@
+#include <string>
+
+int main (int argc, char *argv[])
+{
+	std::string *pstr1 = new std::string [100];
+	std::string *pstr2 = new std::string;
+
+	delete [] pstr1;
+	delete pstr2;
+
+	return 0;
+}
